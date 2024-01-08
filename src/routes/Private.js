fe7d6cd9d80acd71b06e911070836aab34c777c3
Silvio Modesto <silviomodesto2@gmail.com>
2024-01-08 17:22:23 -0400

    alteração para deploy

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -1,48 +1,46 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import { auth } from '../firebaseConnection'
-import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebaseConnection';
+import { onAuthStateChanged } from 'firebase/auth';
 
-import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 
-export default function Private({ children }){
+export default function Private({ children }) {
   const [loading, setLoading] = useState(true);
   const [signed, setSigned] = useState(false);
 
   useEffect(() => {
-    async function checkLogin(){
+    async function checkLogin() {
       const unsub = onAuthStateChanged(auth, (user) => {
-         //se tem user logado
-         if(user){
+        if (user) {
           const userData = {
             uid: user.uid,
             email: user.email,
-          }
+          };
 
-          localStorage.setItem("@detailUser", JSON.stringify(userData))
+          localStorage.setItem('@detailUser', JSON.stringify(userData));
 
           setLoading(false);
           setSigned(true);
+        } else {
+          setLoading(false);
+          setSigned(false);
+        }
+      });
 
-         } else{
-           //nao possui user logado
-           setLoading(false);
-           setSigned(false);
-         }
-      })
+      //cleanup para desconectar o listener quando o componente for desmontado
+      return () => unsub();
     }
 
     checkLogin();
-  }, [])
+  }, []);
 
-  if(loading){
-    return(
-      <div></div>
-    )
+  if (loading) {
+    return <div></div>;
   }
 
-  if(!signed){
-    return <Navigate to="/"/>
+  if (!signed) {
+    return <Navigate to="/" />;
   }
 
   return children;
